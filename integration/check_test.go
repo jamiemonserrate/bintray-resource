b5806a7f4cf674a157d96468c76973c59133e9b3
Jamie Monserrate <jmonserrate@pivotal.io>
2015-06-05 14:42:49 +0100

    Refactoring test to improve readability

--- integration/check_test.go
+++ integration/check_test.go
@@ -1,81 +1,68 @@
 package integration_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"os/exec"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-	"os/exec"
-	"time"
-	"bytes"
-	"encoding/json"
 
 	"github.com/jamiemonserrate/bintray-resource/check"
 )
 
-var _ = Describe("check", func(){
+var checkPath string
+
+var _ = Describe("check", func() {
 	var (
-		checkPath string
 		err error
-		session *gexec.Session
 	)
 
-	BeforeEach(func(){
+	BeforeEach(func() {
 		checkPath, err = gexec.Build("github.com/jamiemonserrate/bintray-resource/cmd/check")
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("returns empty array if the version provided is the latest", func(){
-		checkRequest := check.CheckRequest{
-			Version: check.Version{
-				Number: "2.1.1",
-			},
-		}
-		stdin := &bytes.Buffer{}
-		err := json.NewEncoder(stdin).Encode(checkRequest)
-		Ω(err).ShouldNot(HaveOccurred())
-		
-		command := exec.Command(checkPath)
-		command.Stdin = stdin
-		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
+	It("returns empty array if the version provided is the latest", func() {
+		response := execCheckCommandWith(check.CheckRequest{
+			Version: check.Version{Number: "2.1.1"},
+		})
 
-		Eventually(session, 5*time.Second).Should(gexec.Exit(0))
-		reader := bytes.NewBuffer(session.Buffer().Contents())
-
-		var response check.CheckResponse
-		err = json.NewDecoder(reader).Decode(&response)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		Ω(response).Should(BeEmpty())
+		Expect(response).To(BeEmpty())
 	})
 
-	It("returns the version when there is a version greater than the input", func(){
-		checkRequest := check.CheckRequest{
-			Version: check.Version{
-				Number: "2.1.0",
-			},
-		}
-		stdin := &bytes.Buffer{}
-		err := json.NewEncoder(stdin).Encode(checkRequest)
-		Ω(err).ShouldNot(HaveOccurred())
-		
-		command := exec.Command(checkPath)
-		command.Stdin = stdin
-		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
+	It("returns the version when there is a version greater than the input", func() {
+		response := execCheckCommandWith(check.CheckRequest{
+			Version: check.Version{Number: "2.1.0"},
+		})
 
-		Eventually(session, 5*time.Second).Should(gexec.Exit(0))
-		reader := bytes.NewBuffer(session.Buffer().Contents())
-
-		var response check.CheckResponse
-		err = json.NewDecoder(reader).Decode(&response)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		Ω(response).Should(Equal(check.CheckResponse{
-			{
-				Number: "2.1.1",
-			},
-		}))
+		Expect(response).To(Equal(check.CheckResponse{{Number: "2.1.1"}}))
 	})
 })
 
+func execCheckCommandWith(checkRequest check.CheckRequest) check.CheckResponse {
+	command := exec.Command(checkPath)
+	command.Stdin = encodeJson(checkRequest)
+
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 5*time.Second).Should(gexec.Exit(0))
+
+	return decodeJson(session.Buffer().Contents())
+}
+
+func encodeJson(checkRequest check.CheckRequest) *bytes.Buffer {
+	encodedJson := &bytes.Buffer{}
+	err := json.NewEncoder(encodedJson).Encode(checkRequest)
+	Expect(err).ToNot(HaveOccurred())
+	return encodedJson
+}
+
+func decodeJson(encodedResponse []byte) check.CheckResponse {
+	decodedResponse := check.CheckResponse{}
+	err := json.NewDecoder(bytes.NewBuffer(encodedResponse)).Decode(&decodedResponse)
+	Expect(err).ToNot(HaveOccurred())
+	return decodedResponse
+}
